86b8267dbb6bd78619b372641258d645252a6527
Muhammad Ridwan Solehudin Fadilah <144208656+ridwanfadilah@users.noreply.github.com>
2026-08-05 20:38:57 +0700

    Create baru-belajar-1.java

---
+++ Praktikum 1/baru-belajar-1.java
@@ -0,0 +1,8 @@
+package Praktikum1;
+
+public class BaruBelajar {
+    public static void main (String[] args){
+        System.out.println("Bismillaahirrahmaanirrahiim");
+        System.out.println("Belajar Java itu asyiik...");
+    }
+}
