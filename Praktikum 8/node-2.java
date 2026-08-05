a03bfa8e564a40baae2df6ddf409a5a461ffb7f6
Muhammad Ridwan Solehudin Fadilah <144208656+ridwanfadilah@users.noreply.github.com>
2026-08-05 20:45:29 +0700

    Create node-2.java

---
+++ Praktikum 8/node-2.java
@@ -0,0 +1,14 @@
+package Praktikum8;
+public class Node2 {
+    public long data;
+    public Node2 next;
+    public Node2 prev;
+    //------------------------------
+    public Node2 (long d){
+        data = d;
+    }
+    //------------------------------
+    public void displayNode2() {
+        System.out.println(data + " ");
+    }
+}
