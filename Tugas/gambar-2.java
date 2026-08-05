e579de1b8e9a49a3dd94dbe162f99f4ff310f1c1
Muhammad Ridwan Solehudin Fadilah <144208656+ridwanfadilah@users.noreply.github.com>
2026-08-05 20:49:28 +0700

    Create gambar-2.java

---
+++ Tugas/gambar-2.java
@@ -0,0 +1,11 @@
+package Tugas1;
+
+public class Gambar2 {
+    public static void main (String[] args){
+        System.out.println("        1");
+        System.out.println("      2 1");
+        System.out.println("    3 2 1");
+        System.out.println("  4 3 2 1");
+        System.out.println("5 4 3 2 1");
+    }
+}
