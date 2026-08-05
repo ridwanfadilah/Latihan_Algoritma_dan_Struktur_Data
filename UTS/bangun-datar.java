18ef933ce3c0a14df04bd02c87aac934531cc207
Muhammad Ridwan Solehudin Fadilah <144208656+ridwanfadilah@users.noreply.github.com>
2026-08-05 20:51:52 +0700

    Create bangun-datar.java

---
+++ UTS/bangun-datar.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+public class BangunDatar {
+    public static void garis(){
+        for(int i=1; i <=30; i ++)
+            System.out.print("-");
+        System.out.println();
+    }
+    public static void menu(){
+        System.out.println("1. Persegi");
+        System.out.println("2. Segitiga Siku");
+        System.out.println("3. Lingkaran");
+    }
+    public static double lingkaran(double jari){
+        final double pi = 3.14;
+        double luas = pi*Math.pow(jari, 2);
+        return luas;
+    }
+    public static void tampil (double lu){
+        System.out.println("Luasnya adalah = " + lu);
+    }
+    public static double persegi (double sisi){
+        double luas = sisi*sisi;
+        return luas;
+    }
+    private static double segitiga (double alas, double tinggi){
+        double luas = (alas*tinggi)/2;
+        return luas;
+    }
+    private static double sisimiring (double alas, double tinggi){
+        double simir = Math.sqrt(alas*alas + tinggi*tinggi);
+        return simir;
+    }
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        menu();
+        System.out.println("Pilihan anda : ");
+        int p = scn.nextInt();
+        if (p==1){
+            garis();
+            System.out.println("Menghitung Luas Persegi");
+            garis();
+            System.out.println("Masukkan panjang sisi persegi = ");
+            double s = scn.nextDouble();
+            double l = persegi(s);
+            tampil(l);
+        }
+        else if (p==2){
+            garis();
+            System.out.println("Menghitung Luas Segitiga Siku-Siku");
+            garis();
+            System.out.println("Masukkan panjang alas = ");
+            double a = scn.nextDouble();
+            System.out.println("Masukkan nilai tinggi = ");
+            double t = scn.nextDouble();
+            double l = segitiga(a,t);
+            tampil(l);
+        }
+        else{
+            garis();
+            System.out.println("Menghitung Luas Lingkaran");
+            garis();
+            System.out.println("Masukkan nilai jari-jari = ");
+            double j = scn.nextDouble();
+            double l = lingkaran(j);
+            tampil(l);
+        }
+        garis();
+        System.out.println("Terima Kasih");
+    }
+}
