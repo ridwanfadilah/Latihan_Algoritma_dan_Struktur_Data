a64338d94d6eb464394174e537f4cc0fa5ef3d1a
Muhammad Ridwan Solehudin Fadilah <144208656+ridwanfadilah@users.noreply.github.com>
2026-08-05 20:46:21 +0700

    Create node-3.java

---
+++ Praktikum 9/node-3.java
@@ -0,0 +1,12 @@
+package Praktikum9;
+public class Node3 {
+    public int iData;
+    public Node3 leftChild;
+    public Node3 rightChild;
+
+    public void displayNode3() {        //method untuk menampilkan data
+        System.out.print("(");
+        System.out.print(iData);
+        System.out.print(")");
+    }
+}
