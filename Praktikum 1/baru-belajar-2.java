bef94fbbc3e847ba9577030710812580ea84e6ae
Muhammad Ridwan Solehudin Fadilah <144208656+ridwanfadilah@users.noreply.github.com>
2026-08-05 20:39:23 +0700

    Create baru-belajar-2.java

---
+++ Praktikum 1/baru-belajar-2.java
@@ -0,0 +1,10 @@
+package Praktikum1;
+
+public class BaruBelajar2 {
+    public static void main (String[] args){
+        System.out.println("Bismillah, saya belajar bahasa program java");
+        System.out.println("Java seperti nama salah satu pulau di Indonesia");
+        System.out.println("Java sangat mudah dan menyenangkan");
+        System.out.println("Saya suka bahasa java");
+    }
+}
