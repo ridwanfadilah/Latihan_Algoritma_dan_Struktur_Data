ec9fe3468b69421c44729693b0f702fb9b5f432a
Muhammad Ridwan Solehudin Fadilah <144208656+ridwanfadilah@users.noreply.github.com>
2026-08-05 20:26:11 +0700

    Create angka-1.java

---
+++ Modul 1/angka-1.java
@@ -0,0 +1,9 @@
+package Latihan1;
+
+public class Angka1 {
+    public static void main(String[] args){
+        System.out.printf(" %d %d %d %d %d \n", 1, 2, 3, 4, 5);
+        System.out.printf(" %d %d %d %d %d \n", 6, 7, 8, 9, 10);
+        System.out.printf(" %d %d %d %d %d \n", 11, 12, 13, 14, 15);
+    }
+}
